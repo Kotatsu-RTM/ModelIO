10fa8f8acb0f5def22097dc8c3669016f851beee
Siro_256 <siro@siro256.dev>
2023-09-14 00:22:35 +0900

    Override equals and hashCode of Vector2f and Vector3f

--- src/main/kotlin/dev/siro256/modelio/Model.kt
+++ src/main/kotlin/dev/siro256/modelio/Model.kt
@@ -27,11 +27,41 @@ data class Model(
     data class Vector2f(
         val x: Float,
         val y: Float,
-    )
+    ) {
+        /**
+         * [Why add 0.0f?](https://twitter.com/i/status/1701898660140003510)
+         */
+        override fun equals(other: Any?) =
+            other is Vector2f &&
+                    (other.x + 0.0f).equals(x + 0.0f) &&
+                    (other.y + 0.0f).equals(y + 0.0f)
+
+        override fun hashCode(): Int {
+            var result = x.hashCode()
+            result = 31 * result + y.hashCode()
+            return result
+        }
+    }
 
     data class Vector3f(
         val x: Float,
         val y: Float,
         val z: Float,
-    )
+    ) {
+        /**
+         * [Why add 0.0f?](https://twitter.com/i/status/1701898660140003510)
+         */
+        override fun equals(other: Any?) =
+            other is Vector3f &&
+                    (other.x + 0.0f).equals(x + 0.0f) &&
+                    (other.y + 0.0f).equals(y + 0.0f) &&
+                    (other.z + 0.0f).equals(z + 0.0f)
+
+        override fun hashCode(): Int {
+            var result = x.hashCode()
+            result = 31 * result + y.hashCode()
+            result = 31 * result + z.hashCode()
+            return result
+        }
+    }
 }
